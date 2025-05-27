2a4a468c16c9f03dafadf755caf046da25bead64
0000asif <asasif8899@gmail.com>
2025-05-27 23:56:22 +0600

    dice

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -1,10 +1,41 @@
+import { useState } from 'react';
+import styled from 'styled-components';
 
 const RollDice = () => {
-   return (
-      <div>
-         <img src="./images/cube.png" alt="cube" />
+  const [CurrentDice, setCurrentDice] = useState('1');
+
+  const generateRandomDice = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  const rollDice = () => {
+    const randomNumber = generateRandomDice(1, 7);
+    setCurrentDice( randomNumber);
+  };
+
+  return (
+    <Discontainer>
+      <div className="dice" onClick={rollDice}>
+        <img src={`./images/dice_${CurrentDice}.png`} alt="cube" />
       </div>
-   );
+      <p>Click on Dice to roll</p>
+    </Discontainer>
+  );
 };
 
 export default RollDice;
+
+const Discontainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .dice {
+    cursor: pointer;
+  }
+
+  p {
+    font-size: 24px;
+    font-weight: 500;
+  }
+`;
